2eea018b838828c0867d11b53dc47838dc2d5dfd
J-o-y-a-l <joyaljose@674@gmail.com>
2025-12-20 16:12:54 +0530

    prime added

---
+++ prime/prime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int number = 7;
+        bool isPrime = true;
+
+        if (number <= 1)
+            isPrime = false;
+
+        for (int i = 2; i <= number / 2 && isPrime; i++)
+        {
+            if (number % i == 0)
+                isPrime = false;
+        }
+        Console.WriteLine(isPrime
+            ? $"{number} is a Prime number"
+            : $"{number} is not a Prime number");
+    }
+}
