2eaf24abcf2c7f51a52a5a3551bfea51a079402e
J-o-y-a-l <joyaljose@674@gmail.com>
2025-12-20 16:19:14 +0530

    diamond pattern added

---
+++ diamond pattern/diamond pattern/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class GFG
+{
+
+    static void printDiamond(int n)
+    {
+        int space = n - 1;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < space; j++)
+                Console.Write(" ");
+
+            for (int j = 0; j <= i; j++)
+                Console.Write("* ");
+
+            Console.Write("\n");
+            space--;
+        }
+        space = 0;
+        for (int i = n; i > 0; i--)
+        {
+
+            for (int j = 0; j < space; j++)
+                Console.Write(" ");
+
+            for (int j = 0; j < i; j++)
+                Console.Write("* ");
+
+            Console.Write("\n");
+            space++;
+        }
+    }
+    public static void Main()
+    {
+        printDiamond(5);
+
+    }
+}
