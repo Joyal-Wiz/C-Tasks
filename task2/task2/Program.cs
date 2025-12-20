8d8645f240c25086fdadc7e585e6f4628ef37a1d
J-o-y-a-l <joyaljose@674@gmail.com>
2025-12-20 14:26:09 +0530

    task1,2,3,4,5

---
+++ task2/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void evenornot(int x)
+    {
+        if (x < 0)
+        {
+            Console.WriteLine("negative");
+        } else if (x>=1) {
+            Console.WriteLine("positive");
+        }
+        else
+        {
+            Console.WriteLine("zero");
+        }
+    }
+    static void Main()
+    {
+        evenornot(-1);
+    }
+}
---
+++ tasks/tasks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void evenornot(int x){
+        if (x % 2 == 0)
+        {
+            Console.WriteLine("even");
+        }
+        else {
+            Console.WriteLine("odd");
+        }
+    }
+    static void Main()
+    {
+        evenornot(1);
+    }
+}
