670013da14e5cd1e1099c93778aae222cb19e50b
J-o-y-a-l <joyaljose@674@gmail.com>
2025-12-21 20:05:53 +0530

    day 3

---
+++ day 3/day 3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Animal
+{
+    public string Name;
+    public int Age;
+
+    public void Speak()
+    {
+        Console.WriteLine("Name: " + Name + ", Age: " + Age);
+    }
+}
+
+
+class Dog : Animal
+{
+    public string Breed;
+}
+
+
+class Cat : Animal
+{
+    public void Meow()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     
+        Dog dog = new Dog();
+        dog.Name = "Buddy";
+        dog.Age = 3;
+        dog.Breed = "Golden Retriever";
+        dog.Speak();
+
+        Cat cat = new Cat();
+        cat.Name = "Whiskers";
+        cat.Age = 2;
+        cat.Speak();
+        cat.Meow();
+    }
+}
